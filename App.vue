eee7691d718c5120906087115f1374afdb27f6a3
aswinpreman06 <66995936+aswinpreman06@users.noreply.github.com>
2023-07-26 13:35:28 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="widget">
+     <button @click='check()'>Chat Widget</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  methods:{
+    check(){
+      alert('chat Initialized')
+    }
+  }
+  
+}
+</script>
+
+<style>
+#widget {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  position:relative;
+  top:500px;
+  left:540px;
+}
+</style>
